b40f87a2dfaf0efc9554a83d3290a04fc74755ab
Alexander Kachkaev <alexander.kachkaev@woodsbagot.com>
2016-09-23 12:16:23 +0100

    Refactor
    - use alpine nginx
    - remove client_max_body_size limit
    
    Fixes #1

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-/bin/sed -i "s/<proxy_pass_placeholder>/${TARGET_SERVER}/" /etc/nginx/sites-enabled/default
+/bin/sed -i "s/<proxy_pass_placeholder>/${TARGET_SERVER}/" /etc/nginx/conf.d/default.conf
 
 nginx -g "daemon off;"
