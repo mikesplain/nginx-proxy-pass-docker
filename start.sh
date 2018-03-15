80fb9cdb2f308b27b812925c8a43e041b463874d
Mike Splain <mike.splain@gmail.com>
2018-03-14 22:18:29 -0400

    Add host placeholder

--- start.sh
+++ start.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
+
+HOST_SERVER=${HOST_SERVER:-\$host}
+
 /bin/sed -i "s/<proxy_pass_placeholder>/${TARGET_SERVER}/" /etc/nginx/conf.d/default.conf
+/bin/sed -i "s/<host_placeholder>/${HOST_SERVER}/" /etc/nginx/conf.d/default.conf
 
 nginx -g "daemon off;"
