23f3d39024da5b981fbf3718ceb6070ea55a2d82
Guy Sheffer <guysoft@gmail.com>
2018-12-09 00:42:37 +0200

    Fix path to match new format

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 
 HOST_SERVER=${HOST_SERVER:-\$host}
 
-/bin/sed -i "s/<proxy_pass_protocol_placeholder>/${HTTP_PROTOCOL}/" /etc/nginx/sites-enabled/default.conf
+/bin/sed -i "s/<proxy_pass_protocol_placeholder>/${HTTP_PROTOCOL}/" /etc/nginx/conf.d/default.conf
 /bin/sed -i "s/<proxy_pass_placeholder>/${TARGET_SERVER}/" /etc/nginx/conf.d/default.conf
 /bin/sed -i "s/<host_placeholder>/${HOST_SERVER}/" /etc/nginx/conf.d/default.conf
 
